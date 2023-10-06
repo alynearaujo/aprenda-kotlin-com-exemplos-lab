1dc82b2f15a497f9912e627c53663e55071ff1f5
Alyne Araújo <121644691+alynearaujo@users.noreply.github.com>
2023-10-05 22:22:06 -0300

    Resolução do desafio concluída.

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,48 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel = Nivel.BASICO)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("${usuario.nome} matriculado na formação $nome")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criando alguns conteúdos educacionais com diferentes níveis de dificuldade
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 45, Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 60, Nivel.INTERMEDIARIO)
+    val conteudo3 = ConteudoEducacional("Desenvolvimento Android", 90, Nivel.DIFICIL)
+
+    // Criando uma formação com esses conteúdos
+    val formacaoKotlin = Formacao("Formação Kotlin", listOf(conteudo1, conteudo2, conteudo3))
+
+    // Criando alguns usuários
+    val usuario1 = Usuario("Amanda")
+    val usuario2 = Usuario("Tânia")
+    val usuario3 = Usuario("Carol")
+
+    // Matriculando os usuários na formação
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+    formacaoKotlin.matricular(usuario3)
+
+    // Verificando os inscritos na formação
+    println("Inscritos na formação ${formacaoKotlin.nome}:")
+    for (inscrito in formacaoKotlin.inscritos) {
+        println(inscrito.nome)
+    }
+
+    // Teste do nível de dificuldade dos conteúdos
+    println("\nNível de dificuldade dos conteúdos:")
+    for (conteudo in formacaoKotlin.conteudos) {
+        println("${conteudo.nome}: ${conteudo.nivel}")
+    }
 }
